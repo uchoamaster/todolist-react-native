9fbddbe17e659dedb8243a6da660230bb68680a3
Carlos Uchoa <carlosjuchoa@hotmail.com>
2021-08-21 23:44:10 -0300

    criando o projeto todolist e começando a implementar o css e o component

---
+++ TodoApp/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import {  SafeAreaView, StyleSheet } from 'react-native';
+const COLORS = {primary: '#1f145c', white: '#fff'};
+
+const App = () => {
+  return <SafeAreaView style={{flex: 1, backgroundColor: COLORS.white}}></SafeAreaView>
+  };
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
