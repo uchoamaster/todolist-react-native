d269df5ff1fb86acc1798825ba48bd7e44222425
Carlos Uchoa <carlosjuchoa@hotmail.com>
2021-08-21 23:58:35 -0300

    aplicando css e posicionamento dos elementos no header

--- TodoApp/App.js
+++ TodoApp/App.js
@@ -1,13 +1,38 @@
 import React from 'react';
 
-import {  SafeAreaView, StyleSheet } from 'react-native';
-const COLORS = {primary: '#1f145c', white: '#fff'};
+import {  SafeAreaView, StyleSheet, View, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+const COLORS = {primary: '#1f145c', white: '#fff', red: '#ff0000'};
 
 const App = () => {
-  return <SafeAreaView style={{flex: 1, backgroundColor: COLORS.white}}></SafeAreaView>
+  return ( 
+  <SafeAreaView style={{flex: 1, backgroundColor: COLORS.white}}>
+    <View style={styles.header}>
+      <Text style={styles.headerText}>
+        TODOAPP
+      </Text>
+      <Icon name="delete" style={styles.Icon}/>
+    </View>
+  </SafeAreaView>
+    );
   };
 
 const styles = StyleSheet.create({
+  header: {
+    padding: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  headerText: {
+  color: COLORS.primary,
+  fontWeight: 'bold',
+  fontSize: 20,
+  },
+  Icon: {
+    color: COLORS.red,
+    fontSize: 30,
+  }
 
 });
 
