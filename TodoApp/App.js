b26c3f3079c338d23df3c7dce2a428a9bf17ef7c
Carlos Uchoa <carlosjuchoa@hotmail.com>
2021-08-22 00:15:04 -0300

    ajustando componente no rodapé

--- TodoApp/App.js
+++ TodoApp/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {  SafeAreaView, StyleSheet, View, Text } from 'react-native';
+import {  SafeAreaView, StyleSheet, View, Text , TextInput } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 const COLORS = {primary: '#1f145c', white: '#fff', red: '#ff0000'};
 
@@ -13,6 +13,11 @@ const App = () => {
       </Text>
       <Icon name="delete" style={styles.Icon}/>
     </View>
+    <View style={styles.footer}>
+    <View style={styles.inputContainer}>
+      <TextInput style={styles.input} placeholder="Digite sua tarefa aqui" />
+    </View>
+    </View>  
   </SafeAreaView>
     );
   };
@@ -32,6 +37,25 @@ const styles = StyleSheet.create({
   Icon: {
     color: COLORS.red,
     fontSize: 30,
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    color: COLORS.white,
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  inputContainer: {
+    backgroundColor: COLORS.white,
+    elevation: 40,
+    flex: 1,
+    height: 50,
+    marginVertical: 20,
+    marginRight: 20,
+    borderRadius: 30,
+    paddingHorizontal: 20,
   }
 
 });
